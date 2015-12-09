58c6bac57db9ec16b44f77e328baa58aa7cad651
Geoff Klapproth <GeoffKlapp@users.noreply.github.com>
2015-12-09 13:31:37 -0500

    Update assignment8sliders.js

--- gui1/js/assignment8sliders.js
+++ gui1/js/assignment8sliders.js
@@ -1,1 +1,8 @@
-// Insert some stuff here
+/*
+  File: http://geoffklapp.github.io/websites/gui1/js/assignment8sliders.js
+  Geoff Klapproth, UMass Lowell Computer Science alumni,
+  geoffrey_klapproth@student.uml.edu or geoffklapproth@gmail.com
+  Created on 11/5/15
+  Assignment 8 jQuery file to initialize and use UI sliders
+*/
+

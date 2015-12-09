cadc6c24ce887e565369f6fa569de5607526869b
Geoff Klapproth <GeoffKlapp@users.noreply.github.com>
2015-12-09 18:52:58 -0500

    Update assignment8sliders.js

--- gui1/js/assignment8sliders.js
+++ gui1/js/assignment8sliders.js
@@ -6,7 +6,50 @@
   Assignment 8 jQuery file to initialize and use UI sliders
 */
 
-function {
-  
+function() {
+  $("#sliderRowStart").slider({
+    value: 1,
+    min: -20,
+    max: 20,
+    step: 1,
+    slide: function(event, ui) {
+      $("#rowStart").val(ui.value);
+    }
+  });
 }
 
+function() {
+  $("#sliderRowEnd").slider({
+    value: 1,
+    min: -20,
+    max: 20,
+    step: 1,
+    slide: function(event, ui) {
+      $("#rowEnd").val(ui.value);
+    }
+  });
+}
+
+function() {
+  $("#sliderColStart").slider({
+    value: 1,
+    min: -20,
+    max: 20,
+    step: 1,
+    slide: function(event, ui) {
+      $("#colStart").val(ui.value);
+    }
+  });
+}
+
+function() {
+  $("#sliderColEnd").slider({
+    value: 1,
+    min: -20,
+    max: 20,
+    step: 1,
+    slide: function(event, ui) {
+      $("#colEnd").val(ui.value);
+    }
+  });
+}

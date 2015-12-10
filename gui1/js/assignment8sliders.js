50176b1f2d0051934600e322f19f092b19384379
Geoff Klapproth <GeoffKlapp@users.noreply.github.com>
2015-12-09 22:23:06 -0500

    Update assignment8sliders.js

--- gui1/js/assignment8sliders.js
+++ gui1/js/assignment8sliders.js
@@ -5,51 +5,52 @@
   Created on 11/5/15
   Assignment 8 jQuery file to initialize and use UI sliders
 */
+$(document).ready({
+  function() {
+    $("#sliderRowStart").slider({
+      value: 1,
+      min: 1,
+      max: 20,
+      step: 1,
+      slide: function(event, ui) {
+        $("#rowStart").val(ui.value);
+      }
+    });
+  }
 
-function() {
-  $("#sliderRowStart").slider({
-    value: 1,
-    min: -20,
-    max: 20,
-    step: 1,
-    slide: function(event, ui) {
-      $("#rowStart").val(ui.value);
-    }
-  });
-}
+  function() {
+    $("#sliderRowEnd").slider({
+      value: 1,
+      min: 1,
+      max: 20,
+      step: 1,
+      slide: function(event, ui) {
+        $("#rowEnd").val(ui.value);
+      }
+    });
+  }
 
-function() {
-  $("#sliderRowEnd").slider({
-    value: 1,
-    min: -20,
-    max: 20,
-    step: 1,
-    slide: function(event, ui) {
-      $("#rowEnd").val(ui.value);
-    }
-  });
-}
+  function() {
+    $("#sliderColStart").slider({
+      value: 1,
+      min: -20,
+      max: 20,
+      step: 1,
+      slide: function(event, ui) {
+        $("#colStart").val(ui.value);
+      }
+    });
+  }
 
-function() {
-  $("#sliderColStart").slider({
-    value: 1,
-    min: -20,
-    max: 20,
-    step: 1,
-    slide: function(event, ui) {
-      $("#colStart").val(ui.value);
-    }
-  });
-}
-
-function() {
-  $("#sliderColEnd").slider({
-    value: 1,
-    min: -20,
-    max: 20,
-    step: 1,
-    slide: function(event, ui) {
-      $("#colEnd").val(ui.value);
-    }
-  });
-}
+  function() {
+    $("#sliderColEnd").slider({
+      value: 1,
+      min: -20,
+      max: 20,
+      step: 1,
+      slide: function(event, ui) {
+        $("#colEnd").val(ui.value);
+      }
+    });
+  }
+});
